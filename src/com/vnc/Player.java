14475e845b8c7756b61e7545d033a1ebc232475a
TheBreadEnergy <50107547+TheBreadEnergy@users.noreply.github.com>
2020-01-29 18:55:42 +0300

    Revert "transfer method"
    This reverts commit 202c6197b26c25a216f4b4d898c0c3edd54a5812.

--- src/com/vnc/Player.java
+++ src/com/vnc/Player.java
@@ -8,50 +8,18 @@ public class Player {
     private int y;
     private Scanner sc = new Scanner(System.in);
     private ArrayList<Pair> pairs= new ArrayList<>();
-
-    private void moveOnOuterCircle(int movement){
-        if(x == 0 && y <= 10){
-            if (y + movement <= 10){
-                y += movement;
-            }else{
-                x = (y + movement) - 10;
-                y = 10;
-            }
-        }else if(x <= 10 && y == 10){
-            if (x + movement <= 10){
-                x += movement;
-            }else{
-                movement -= 10 - x;
-                y = 10 - movement;
-                x = 10;
-            }
-        }else if(x == 10 && y <= 10){
-            if (y - movement >= 0){
-                y -= movement;
-            }else{
-                movement -= y;
-                x = 10 - movement;
-                y = 0;
-            }
-        }else if(x <= 10 && y == 0){
-            if (x - movement >= 0){
-                x -= movement;
-            }else{
-                movement -= x;
-                y = movement;
-                x = 0;
-            }
-        }
-    }
+    private short list[] = {2,6,2,7,2,8,3,9,4,10,5,10,6,10,7,10,8,10,9,9,10,8,10,7,10,6,10,5,10,4,9,3,8,2,7,2,6,2,5,2,4,2,3,3,2,4,2,5};
 
     public Player(){
         for (int i = 0; i < 47; i++){
             Pair pair = new Pair();
-            short[] list = {1,5,1,6,1,7,2,8,3,9,4,9,5,9,6,9,7,9,8,8,9,7,9,6,9,5,9,4,9,3,8,2,7,1,6,1,5,1,4,1,3,1,2,2,1,3,1,4,1,5};
-            pair.makePair(list[i], list[i+1]);
+            pair.makePair(list[i] - 1, list[i+1] - 1);
             pairs.add(pair);
             i += 1;
         }
+//        for(int i = 0; i < 24; i++){
+//            pairs.get(i).print();
+//        }
     }
 
     public void move(int movement){
@@ -196,118 +164,6 @@ public class Player {
                     x = 0;
                 }
             }
-        }else{
-            for (int i = 0; i < pairs.size(); i++){
-                if (pairs.get(i).getX() == x && pairs.get(i).getY() == y){
-
-                    if (x >= 1 && x <= 4 && y >= 5 && y <= 9 && i + movement >= 6) {
-                        int answer;
-                        System.out.println("Хотите перейти на другой круг?");
-                        System.out.println("1 - да");
-                        System.out.println("0 - нет");
-                        answer = sc.nextInt();
-
-                        switch (answer){
-                            case(1):
-                                movement = movement - (6 - i) - 1;
-                                x = 5;
-                                y = 10;
-                                moveOnOuterCircle(movement);
-                                break;
-                            case(2):
-                                if (i + movement <= pairs.size()){
-                                    x = pairs.get(i + movement).getX();
-                                    y = pairs.get(i + movement).getY();
-                                }else {
-                                    x = pairs.get(i + movement - pairs.size()).getX();
-                                    y = pairs.get(i + movement - pairs.size()).getY();
-                                }
-                                break;
-                        }
-                    } else if (x >= 5 && x <= 9 && y <= 9 && y >= 6 && i + movement >= 6){
-                        int answer;
-                        System.out.println("Хотите перейти на другой круг?");
-                        System.out.println("1 - да");
-                        System.out.println("0 - нет");
-                        answer = sc.nextInt();
-
-                        switch (answer){
-                            case(1):
-                                movement = movement - (6 - i) - 1;
-                                x = 5;
-                                y = 10;
-                                moveOnOuterCircle(movement);
-                                break;
-                            case(2):
-                                if (i + movement <= pairs.size()){
-                                    x = pairs.get(i + movement).getX();
-                                    y = pairs.get(i + movement).getY();
-                                }else {
-                                    x = pairs.get(i + movement - pairs.size()).getX();
-                                    y = pairs.get(i + movement - pairs.size()).getY();
-                                }
-                                break;
-                        }
-                    }else if(x <=9 && x >= 6 && y <= 5 && y >= 1 && i + movement >= 12){
-                        int answer;
-                        System.out.println("Хотите перейти на другой круг?");
-                        System.out.println("1 - да");
-                        System.out.println("0 - нет");
-                        answer = sc.nextInt();
-
-                        switch (answer){
-                            case(1):
-                                movement = movement - (12 - i) - 1;
-                                y = 0;
-                                x = 5;
-                                moveOnOuterCircle(movement);
-                                break;
-                            case(0):
-                                if (i + movement <= pairs.size()){
-                                    x = pairs.get(i + movement).getX();
-                                    y = pairs.get(i + movement).getY();
-                                }else {
-                                    x = pairs.get(i + movement - pairs.size()).getX();
-                                    y = pairs.get(i + movement - pairs.size()).getY();
-                                }
-                                break;
-                        }
-                    }else if(x >= 1 && x <= 5 && y >= 1 && y <= 4 && i + movement >= 18){
-                        int answer;
-                        System.out.println("Хотите перейти на другой круг?");
-                        System.out.println("1 - да");
-                        System.out.println("0 - нет");
-                        answer = sc.nextInt();
-
-                        switch (answer){
-                            case(1):
-                                movement = movement - (24 - i) - 1;
-                                y = 5;
-                                x = 0;
-                                moveOnOuterCircle(movement);
-                                break;
-                            case(0):
-                                if (i + movement <= pairs.size()){
-                                    x = pairs.get(i + movement).getX();
-                                    y = pairs.get(i + movement).getY();
-                                }else {
-                                    x = pairs.get(i + movement - pairs.size()).getX();
-                                    y = pairs.get(i + movement - pairs.size()).getY();
-                                }
-                                break;
-                        }
-                    }else{
-                        if (i + movement <= pairs.size()){
-                            x = pairs.get(i + movement).getX();
-                            y = pairs.get(i + movement).getY();
-                        }else {
-                            x = pairs.get(i + movement - pairs.size()).getX();
-                            y = pairs.get(i + movement - pairs.size()).getY();
-                        }
-                    }
-                    break;
-                }
-            }
         }
     }
 
@@ -322,8 +178,12 @@ public class Player {
     }
 
     public void getCurrentPosition(){
+        x++;
+        y++;
         System.out.println("x "+ x);
-        System.out.println("y "+ y);
+        System.out.println("y "+y);
+        x--;
+        y--;
     }
 
     public int getX() {
